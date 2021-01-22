81d6986c19c13662dfd3a42f2469d1d875f94f07
CallumCB <77706780+CallumCB@users.noreply.github.com>
2021-01-22 14:17:15 -0700

    Add files via upload

---
+++ DisBotV.1beta.CallumCB.py
@@ -0,0 +1,32 @@
+import random
+import discord
+from discord.ext import commands
+
+#intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
+#client = commands.Bot(command_prefix = '*', intents = intents)
+
+pp = commands.Bot(command_prefix='?')
+
+@pp.event
+async def on_ready():
+    print('Ready and at your service')
+
+@pp.command()
+async def hi(ctx):
+    await ctx.send("Hello!")
+
+@pp.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+@pp.event
+async def on_member_join(member):
+    print('f {member} has joined a server')
+
+@pp.event
+async def on_member_remove(member):
+    print('f {member}has left a server')
+
+
+
+pp.run('ODAxNTY4ODczMjE2MjEzMDAy.YAilLw.hdloPN5njaU2b5dHMsoJ17inwdY')
