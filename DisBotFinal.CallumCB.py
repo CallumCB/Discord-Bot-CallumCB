168065b0f910f82ced42e4096c1ad6e1dc0581df
CallumCB <77706780+CallumCB@users.noreply.github.com>
2021-02-17 20:29:15 -0700

    Update and rename DisBotV.1beta.CallumCB.py to DisBotFinal.CallumCB.py

--- DisBotFinal.CallumCB.py
+++ DisBotFinal.CallumCB.py
@@ -1,4 +1,3 @@
-import random
 import discord
 from discord.ext import commands
 TOKEN=''
@@ -21,29 +20,37 @@ async def hi(ctx):
     await ctx.send("Hello!")
 @pp.command()
 async def ping(ctx):
-    await ctx.send(f'{round(pp.latency * 1000)}ms')
-
+    await ctx.send(f'My latency is {round(pp.latency * 1000)}ms')
+#---
 @pp.command()
+@commands.has_permissions(manage_messages=True)
 async def clear(ctx, amount=1):
     await ctx.channel.purge(limit=amount+1)
     await ctx.send(f'{amount} messages cleared')
 @pp.command()
+@commands.has_permissions(manage_messages=True)
 async def silent_clear(ctx, amount=1):
     await ctx.channel.purge(limit=amount+1)
 @pp.command()
-async def purge_channel(ctx, amount=1000):
+@commands.has_permissions(manage_messages=True)
+async def purge_channel(ctx, amount=200):
     await ctx.channel.purge(limit=amount+1)
-    await ctx.send('This channel has been purged and 1000 messages deleted.')
-
+    await ctx.send('This channel has been purged and 200 messages deleted.')
+#---
+def if_the_admin(ctx):
+    return ctx.author.id == <><><><>SERVER ADMIN ID HERE<><><><>
 @pp.command()
+@commands.check(if_the_admin)
 async def kick(ctx, member : discord.Member, *, reason=None, user=None):
     await member.kick(reason=reason)
     await ctx.send(f'{member.mention} was kicked because {reason}')
 @pp.command()
+@commands.check(if_the_admin)
 async def ban(ctx, member : discord.Member, *, reason=None):
     await member.ban(reason=reason)
     await ctx.send(f'Banned: {member.mention}')
 @pp.command()
+@commands.check(if_the_admin)
 async def unban(ctx, *, user=None):
     try:
         user = await commands.converter.UserConverter().convert(ctx, user)
@@ -64,25 +71,5 @@ async def unban(ctx, *, user=None):
         await ctx.send("Unbanning failed")
         return
     await ctx.send(f"Unbanned {user.mention}!")
-#-------
-
-@pp.event()
-async def on_message(self, message, member):
-        offensive_names = ['DERAGATORY NAMES HERE']
-
-    messageContent = message.content
-    if len(messageContent) > 0:
-        for word in offensive_names:
-            if word in messageContent:
-                await message.delete()
-                await message.channel.send('You can curse, but no name calling.')
-
-    messageContent = message.content
-    if len(messageContent) > 0:
-        for word in offensive_names:
-            if word in messageContent:
-                await
-
-#--------
-
+#---
 pp.run(TOKEN)
